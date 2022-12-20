7bcfa9839a81acd9321b15f5a331a140c6b0f3e0
Lyubenova <anitaencheva@gmail.com>
2022-12-20 12:43:27 +0100

    tidy up code

--- scripts/LSACplantingforest.R
+++ scripts/LSACplantingforest.R
@@ -21,20 +21,21 @@ library(tidyverse)
 #All Data ------------------------------------------
 #load data
 #df_anal <- load_data(to_envir = FALSE)$df_anal
-load(file="C:/Users/3384022/Desktop/AL/Data to use/revision/imputed.data.RData")
+load(file="C:/Users/3384022/Desktop/AL/Data to use/revision/imputed.data_v5.RData")
 
 #load the documentation with the selected variables
 #selected_variables<-read_xlsx("Anita/LSAC documentation/selected_variables2.xlsx")
 #final_selection<-read_xlsx("Anita/LSAC documentation/final_selection.xlsx")
-final_selection<-read_xlsx("LSAC documentation/selected_variables1.xlsx")
+final_selection<-read_xlsx("LSAC documentation/selected_variables1_v5.xlsx")
 final_selection<-subset(final_selection, !is.na(lvl1.varname))
 
 #load the latent scores
-load(file = "Outputs/LSACcreatingScales_revision/DV latent scores.RData")
-load(file="Outputs/LSACcreatingScales_revision/predictor.scores.RData")
+load(file = "Outputs/LSACcreatingScales/DVscores.RData")
+load(file="Outputs/LSACcreatingScales/cfa.scores.RData")
+predictor.scores<-cfa.scores
 colnames(predictor.scores)<-gsub("[.]", "_", colnames(predictor.scores))
 
-DVscores<-DVscores[,10:12]
+DVscores<-DVscores[,13:16]
 # cfa.vars<-subset(final_selection,method=="CFA" | method=="log.model" & !is.na(lvl1.varname))
 # cfa.vars<-cfa.vars$Variable.Name
 
@@ -45,6 +46,8 @@ single.items<-single.items$Variable.Name
 
 #apply as.factor() to all categorical variables
 facs<-final_selection[final_selection$method=="categorical" & !is.na(final_selection$method),]$Variable.Name
+facs<-final_selection[final_selection$method=="categorical" | is.na(final_selection$method),]$Variable.Name
+
 imp[facs] <- lapply(imp[facs], as.factor)
 
 single.items.df<-imp[,colnames(imp) %in% single.items]
@@ -60,8 +63,8 @@ for(i in 1:ncol(single.items.df)){
 #put toghether  the single-item-predictors, latent-score-predictors and DV latent scores
 df_anal<-cbind(single.items.df, predictor.scores[,-1], DVscores)
 colnames(df_anal)<-gsub("[.]", "_", colnames(df_anal))
-#remove out of home activities because no latent scores were computed
-df_anal<-df_anal[,colnames(df_anal) != "out_of_home_activities"]
+# #remove out of home activities because no latent scores were computed
+# df_anal<-df_anal[,colnames(df_anal) != "out_of_home_activities"]
 
 
 ##summary tables-------------------------
@@ -77,184 +80,115 @@ colnames(df_anal)<-gsub("[.]", "_", colnames(df_anal))
 df_anal<-df_anal[,colnames(df_anal) != "out_of_home_activities"]
 
 #variable names of the DVs
-DVs<-paste0("k", c(12,14,16), "_F")
+DVs<-paste0("k", c(10,12,14,16), "_F")
 
-# Preliminary variable selection ------------------------------------------
 
-library(psych)
-#analyze the number of underlying factors in the 5 waves?
-# --> 1 component
-fa.parallel(DVscores)
-#perform PCA with 1 principal component
-dv <- principal(DVscores, nfactors = 1)
-# (a single) latent variable score on emotion regulation for each individual (across all waves?? how does this make sense?) -- is this kind of the random intercept?
-dv$scores
-#add overall emotion regulation latent score (across all waves) to the data and remove the latent scores of the 5 waves??
-df_pres <- data.frame(dv$scores, df_anal[, -which(colnames(df_anal) %in% DVs)])
-str(df_pres)
-df_pres
-
-library(ranger)
-library(tuneRanger)
-tunetask <- makeRegrTask(data = df_pres, target = "PC1")
-res_tune <- tuneRanger(tunetask, num.trees = 1000, iters = 70, save.file.path = NULL)
-#get the argument values to be put in the ranger() function below
-res_tune$model$learner.model
-
-## Set up interactions -----------------------
-#27:parenting variables
-parenting.vars<-grep("parenting", colnames(df_anal), value = TRUE)
-#Error: Only numeric columns allowed in interaction terms.
-interactions.mod<-paste("PC1~",paste(parenting.vars,"*.", collapse = " + "))
-
-set.seed(57)
-res <- ranger(as.formula(interactions.mod), data = df_pres, importance = "impurity_corrected", mtry = 66, min.node.size = 3, num.trees = 1000) #splitrule = "variance"
-VI <- ranger::importance1_pvalues(res, method = "altmann", formula = as.formula(interactions.mod), data = df_pres)
-
-save(VI, file="Outputs/plantingforest/VI (interactions).RData")
-load("Outputs/plantingforest/VI (interactions).RData") #VI
-VI
-
-sum(VI[,2] < .05)
-sum(VI[,2] < .01)
-
-#preview of the important predictors or moderators
-VI %>% 
-  as.data.frame() %>% 
-  rownames_to_column(var="var") %>%  
-  arrange(-importance) %>% 
-  filter(importance>10)
-
-# #important variables and interactions based on pvalue<.05
-# important<-
-#   VI %>% 
-#   as.data.frame() %>% 
-#   rownames_to_column(var="var") %>%  
-#   arrange(-importance) %>% 
-#   filter(pvalue<0.05) 
-
-#important variables and interactions based on pvalue<.01
-important<-
-  VI %>% 
-  as.data.frame() %>% 
-  rownames_to_column(var="var") %>%  
-  arrange(-importance) %>% 
-  filter(pvalue<0.01) 
-
-#get a vector with (1)all important predictors on their own and (2)variables that are in an important interaction
-important.vars<-str_split(important$var, ":", simplify = TRUE) %>% as.data.frame()
-selected<-unique(c(important.vars$V1, important.vars$V2[important.vars$V2!=""]))
+# RI-CLPM-------------------------------------------------------------------------------------------------
+#unconstrained model
+RICLPM <- '
+  # Create between components (random intercepts)
+  RIx =~ 1*k10_parenting_consistent_mom + 1*k12_parenting_consistent_mom + 1*k14_parenting_consistent_mom + 1*k16_parenting_consistent_mom
+  RIy =~ 1*k10_F + 1*k12_F + 1*k14_F + 1*k16_F
   
-#df_anal before: ncol=87
-#df_anal after: ncol=69
-df_anal<-df_anal[, c(DVs, selected)]
+  # Create within-person centered variables
+  wx1 =~ 1*k10_parenting_consistent_mom
+  wx2 =~ 1*k12_parenting_consistent_mom
+  wx3 =~ 1*k14_parenting_consistent_mom 
+  wx4 =~ 1*k16_parenting_consistent_mom
+  wy1 =~ 1*k10_F
+  wy2 =~ 1*k12_F
+  wy3 =~ 1*k14_F
+  wy4 =~ 1*k16_F
 
-#add interactions to the dataset
-important.colnames<-important[grep(":", important$var),]$var
-df_anal[,important.colnames]<-NA
+  # Estimate lagged effects between within-person centered variables
+  wx2 + wy2 ~ wx1 + wy1
+  wx3 + wy3 ~ wx2 + wy2
+  wx4 + wy4 ~ wx3 + wy3
 
-important.interactions<-important.vars %>% subset(V2 != "")
-
-for(i in 1:nrow(important.interactions)){
-  df_anal[,69+i] <-df_anal[,important.interactions$V1[i]]*df_anal[,important.interactions$V2[i]]
-}
-
-
-#temporary reduction of the dataset to make sure the code runs
-df_anal<-df_anal[,c(DVs,"SDQ_conduct_problems_p2:parenting_efficacy_p2","SDQ_conduct_problems_p2","pedsQL_social:parenting_disagree_relationship_p2",
-                    "pedsQL_social", "parenting_disagree_relationship_p2","parenting_efficacy_p2:depressed_p2",
-                    "parenting_efficacy_p2", "depressed_p2","SDQ_conduct_problems_p2:supportive_co_parenting_p2",
-                    "pedsQL_emotional:parenting_warmth_p1", "pedsQL_emotional","parenting_warmth_p1"
-                    )]
+  # Estimate covariance between within-person centered variables at first wave
+  wx1 ~~ wy1 # Covariance
   
-#change column names because ":" is not accepted in openMX
-colnames(df_anal)<-gsub("[:]", "__x__", colnames(df_anal))
+  # Estimate covariances between residuals of within-person centered variables 
+  # (i.e., innovations)
+  wx2 ~~wy2
+  wx3 ~~ wy3
+  wx4 ~~ wy4
+  
+  # Estimate variance and covariance of random intercepts
+  RIx ~~ RIx
+  RIy ~~ RIy
+  RIx ~~ RIy
 
-predvar <- names(df_anal)[!names(df_anal) %in% DVs]
-length(predvar)
-
-
-# lastsig <- order(VI[,1], decreasing = TRUE)
-# lastsig <- max(which(VI[lastsig, 2] < .05))
-# selected <- rownames(VI)[VI[, 2] < .05]
-# p <- VarImpPlot(res, lastsig)
-# p <- p + 
-#   geom_point(data = data.frame(p$data, sig = (VI[,2] < .05)[as.character(p$data$Variable)]), aes(fill = sig), shape = 21) + 
-#   scale_fill_manual(values = c("FALSE" = "white", "TRUE" = "black"))+
-#   theme(legend.position = "none")
-
-
-# SEM forest --------------------------------------------------------------
-
-# we use latent scores as "observed variables" - is that okay/customary?
-basicgrowth <- lavaan::growth(
-"
-i =~ 1*k12_F + 1*k14_F + 1*k16_F 
-s =~ 0*k12_F + 1*k14_F + 2*k16_F
-q =~ 0*k12_F + 1*k14_F + 4*k16_F
-" , data = df_anal)
-#not identified
+  # Estimate (residual) variance of within-person centered variables
+  wx1 ~~ wx1 # Variances
+  wy1 ~~ wy1 
+  wx2 ~~  wx2 # Residual variances
+  wy2 ~~  wy2 
+  wx3 ~~  wx3 
+  wy3 ~~  wy3 
+  wx4 ~~  wx4 
+  wy4 ~~  wy4 
+'
 
 
-write_yaml(as.list(fitmeasures(basicgrowth)), "fitmeasures_growth.yml")
+#constrained model
+RICLPM.constr <- '
+  # Create between components (random intercepts)
+  RIx =~ 1*k10_parenting_consistent_mom + 1*k12_parenting_consistent_mom + 1*k14_parenting_consistent_mom + 1*k16_parenting_consistent_mom
+  RIy =~ 1*k10_F + 1*k12_F + 1*k14_F + 1*k16_F
+  
+  # Create within-person centered variables
+  wx1 =~ 1*k10_parenting_consistent_mom
+  wx2 =~ 1*k12_parenting_consistent_mom
+  wx3 =~ 1*k14_parenting_consistent_mom 
+  wx4 =~ 1*k16_parenting_consistent_mom
+  wy1 =~ 1*k10_F
+  wy2 =~ 1*k12_F
+  wy3 =~ 1*k14_F
+  wy4 =~ 1*k16_F
 
-# Specifying model linear growth curve model using the openMX code (should be the same model as the lavaan model)
-m0 <- as_ram("
-#fix factor loadings
-i =~ 1*k12_F + 1*k14_F + 1*k16_F 
-s =~ 0*k12_F + 1*k14_F + 2*k16_F
-q =~ 0*k12_F + 1*k14_F + 4*k16_F
+  # Estimate lagged effects between within-person centered variables
+  wx2 + wy2 ~ b2*wx1 + b1*wy1
+  wx3 + wy3 ~ b2*wx2 + b1*wy2
+  wx4 + wy4 ~ b2*wx3 + b1*wy3
 
-#latent means: freely estimated
-i ~ meani*1
-s ~ means*1
-q ~ meanq*1
+  # Estimate covariance between within-person centered variables at first wave
+  wx1 ~~ wp.cov*wy1 # Covariance
+  
+  # Estimate covariances between residuals of within-person centered variables 
+  # (i.e., innovations)
+  wx2 ~~ wp.cov*wy2
+  wx3 ~~ wp.cov*wy3
+  wx4 ~~ wp.cov*wy4
+  
+  # Estimate variance and covariance of random intercepts
+  RIx ~~ RIx
+  RIy ~~ RIy
+  RIx ~~ RIy
 
-#residual variances theta: fixed to be equal across waves
-k12_F ~~ vres*k12_F
-k14_F ~~ vres*k14_F
-k16_F ~~ vres*k16_F
-
-#intercepts nu: fix to 0?
-k12_F   ~ 0*1
-k14_F  ~ 0*1
-k16_F  ~ 0*1
-
-
-#covariances between latent variabels: fixed to 0?
-i ~~ 0*s
-i ~~ 0*q
-s ~~ 0*q
-i ~~ 0*i
-s ~~ 0*s
-q ~~ 0*q")
-
-m0 <- run_mx(m0, data = df_anal)
-table_results(m0)
-table_fit(m0)
-# fitting a single tree using the model
-
-
-#takes too long
-# system.time(
-#   predgrowthtree <- semtree(m0, data = df_anal, control = semtree.control())
-# ) # 856.14
-
-#cl <- makeCluster(40)  # cluster of 2 CPUs created, in parallel
-# A cluster can be stopped when not needed.
-# Note – if you want to restart the cluster,
-# you must use the sfClusterEval functions to load the packages for use with the new cluster.
-#
-# stopCluster(cl)
-# A SEM Forest can be grown in parallel by specifying the cluster to be used:
-#
+  # Estimate (residual) variance of within-person centered variables
+  wx1 ~~ wp.var*wx1 # Variances
+  wy1 ~~ wp.var*wy1 
+  wx2 ~~ wp.resvar*wx2 # Residual variances
+  wy2 ~~ wp.resvar*wy2 
+  wx3 ~~ wp.resvar*wx3 
+  wy3 ~~ wp.resvar*wy3 
+  wx4 ~~ wp.resvar*wx4 
+  wy4 ~~ wp.resvar*wy4 
+'
+RICLPM.fit <- lavaan(RICLPM.constr,
+                     data = df_anal, 
+                     meanstructure = T, 
+                     int.ov.free = T
+)
+summary(RICLPM.fit, standardized = T)
 
 
 ####################### Tuning parameters for semForest algorithm ################################
 
 # setting some controls, current: default method, 1000 trees in forest
 controls <- semforest.control()
-controls$num.trees <- 10 # number of trees to grow
+controls$num.trees <- 5 # number of trees to grow
 controls$sampling <- "bootstrap" # number of trees to grow
 controls$mtry <- floor(sqrt(length(predvar)))
 controls$semtree.control
@@ -263,12 +197,22 @@ controls$semtree.control$min.N <- 50
 controls$semtree.control$method <- "score"
 controls$semtree.control$exclude.heywood <- TRUE
 controls
-
+library(parallel)
 cl<-makeCluster(2)
-res_rf<-semtree::semforest(m0, data = df_anal, control = controls)
+res_rf<-semtree::semforest(RICLPM.fit, data = df_anal, control = controls)
 stopCluster(cl)
 
 saveRDS(res_rf, "Outputs/plantingforest/rf_test1.RData")
+
+
+
+
+## NOT further adapted ## ----------------------------------------------------
+
+
+
+
+
 
 set.seed(78326)
 cl<-makeCluster(2) #change the 2 to your number of CPU cores
---
+++ scripts/old scripts/add_k10.R
@@ -0,0 +1,63 @@
+doc<-read_xlsx("LSAC documentation/full_documentation_with_selection_v3.xlsx")
+#filter the full documentation such that it only contains selected variables
+selected_variables<-subset(doc, !is.na(lvl1.varname))
+
+
+paste0("",doc$`Without age`)
+
+#all selected variables at k12
+all.vars.without.age.k12<-selected_variables$`Without age`
+#subset the corresponding variables from k10
+k10<-doc[doc$`Without age` %in% all.vars.without.age.k12 & doc$File=="K10",]
+#which variables from k12 are not present in k10
+a<-selected_variables[which(!all.vars.without.age.k12 %in% k10$`Without age`),]
+a$lvl1.varname %>% unique()
+
+
+#fill in lvl1 and lvl2.varname, method and Note columns for selecetd k10 variables depending on the corresponding entry from k12
+i<-1
+for( i in 1:nrow(doc[doc$File=="K10",])){
+  print(i)
+  wa.var<-doc[doc$File=="K10",]$`Without age`[i]
+  
+  if(nrow(doc[doc$File=="K12" & doc$`Without age`==wa.var & !is.na(doc$lvl1.varname),c("lvl1.varname", "lvl2.varname", "method", "Note")])==0){
+    doc[doc$File=="K10" & doc$`Without age`==wa.var,c("lvl1.varname", "lvl2.varname", "method", "Note")]<-rep(NA,4)
+  }else{
+    doc[doc$File=="K10" & doc$`Without age`==wa.var,c("lvl1.varname", "lvl2.varname", "method", "Note")]<- doc[doc$File=="K12" & doc$`Without age`==wa.var &!is.na(doc$lvl1.varname),c("lvl1.varname", "lvl2.varname", "method", "Note")]
+  }
+
+}
+
+#remove k12 variables except for SDQ.emot
+sdq.emot.vars1<-grep("SDQ.emot", doc[doc$File=="K12" & !is.na(doc$lvl1.varname),]$lvl1.varname, value = TRUE)
+sdq.emot.vars2<-grep("SDQ.emot", doc[doc$File=="K12" & !is.na(doc$lvl2.varname),]$lvl2.varname, value = TRUE)
+
+doc[doc$File=="K12" & !is.na(doc$lvl1.varname) & !doc$lvl1.varname %in% sdq.emot.vars,
+    c("lvl1.varname", "lvl2.varname", "method", "Note")]<-NA
+
+#fix lvl1 adn lvl2 varname for SDQ.emot in k10
+doc[doc$File=="K10" & doc$lvl1.varname %in% sdq.emot.vars1,]$lvl1.varname<-str_replace(doc[doc$File=="K10" & doc$lvl1.varname %in% sdq.emot.vars1,]$lvl1.varname, "k12","k10")
+doc[doc$File=="K10" & doc$lvl2.varname %in% sdq.emot.vars2,]$lvl2.varname<-str_replace(doc[doc$File=="K10" & doc$lvl2.varname %in% sdq.emot.vars2,]$lvl2.varname, "k12","k10")
+
+#check
+doc[doc$File=="K10" & doc$lvl1.varname %in% grep("SDQ.emot",doc$lvl1.varname, value = TRUE),]$lvl1.varname
+doc[doc$File=="K10" & doc$lvl2.varname %in% grep("SDQ.emot",doc$lvl2.varname, value = TRUE),]$lvl2.varname
+
+
+write_xlsx(doc, path = "LSAC documentation/full_documentation_with_selection_v4.xlsx")
+
+# add wave indicator for parenting variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
